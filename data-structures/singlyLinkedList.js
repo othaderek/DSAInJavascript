358081321025b403817865cde4c3550d92b196ec
othaderek <otha.hernandez@gmail.com>
2020-02-18 15:09:24 -0500

    Started Singly Linked List.

---
+++ data-structures/singlyLinkedList.js
@@ -0,0 +1,13 @@
+// Singly Linked List
+
+class Node{
+
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+
+}
+
+let node1 = new Node(1)
+let node2 = new Node(2)
