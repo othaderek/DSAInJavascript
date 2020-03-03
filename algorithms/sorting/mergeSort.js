5c8de90cddd7c5a6e79d86cb052d325a45e1c35b
othaderek <otha.hernandez@gmail.com>
2020-03-03 13:07:36 -0500

    Added mergeSort, incomplete. Creating merge helper function.

---
+++ algorithms/sorting/mergeSort.js
@@ -0,0 +1,12 @@
+// Merge Sort
+
+// Create a helper function that merges sorted arrays in sorted order
+const merge = (arr1, arr2) => {
+    let sortedArray = [];
+    
+    
+}
+
+
+merge([1,3,6,7,8],[2,5,8,13,20,22]);
+
