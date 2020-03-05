dbd726faaafde200ab6e3f342eaaedbf3a20d191
othaderek <otha.hernandez@gmail.com>
2020-03-05 17:25:53 -0500

    adds stuff.

--- algorithms/sorting/mergeSort.js
+++ algorithms/sorting/mergeSort.js
@@ -4,9 +4,18 @@
 // Create mergeSort function that recursively slices array size down until it is 1 or 0 length
 // and uses the merge function to merge the arrays in sorted order until we have one sorted array.
 
+
+// Merge sort can be pretty 'mind bendy' so I'm gonna walk through what it is 
+// on a high level and how I've chosen to implement it here.
+
+// Merge Sort is an efficient sorting algorithm that uses divide and conquer.
+// The way it works is it splits up an array into smaller arrays that are then
+// compared. The array is broken all the way down to arrays of length 1 or 0 and then
+// compared and merged.
+
+// I'm goin to break each line down one by one.
+
 const merge = (arr1, arr2) => {
-    console.log('merging');
-    
     let sortedArray = [];
     let i = 0;
     let j = 0;
@@ -46,4 +55,3 @@ const mergeSort = (array) => {
 }
 
 mergeSort([1,5,34,2,9,-3,100])
-
