26fd6271ecfb5aab11e94726bec0624faa85a1fc
othaderek <otha.hernandez@gmail.com>
2020-03-06 17:04:17 -0500

    Singly Linked List push method added.

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -9,5 +9,28 @@ class Node{
 
 }
 
+class SinglyLinkedList{
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value){
+        let node = new Node(value)
+        if (!this.head){
+            this.head = node
+            this.tail = this.head
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this
+    }
+
+}
+
 let node1 = new Node(1)
 let node2 = new Node(2)
