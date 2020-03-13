7c6eb92fb0fbec8ed5658baf057c4466fe6f3a8b
othaderek <otha.hernandez@gmail.com>
2020-03-12 20:28:06 -0400

    Started Stack class.

---
+++ data-structures/stack.js
@@ -0,0 +1,24 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    push(value){
+        if (!this.first){
+            
+        }
+    }
+
+    pop(){
+
+    }
+}
