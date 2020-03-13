998b8e68356bd9c890676b20e2f6c3458d17ee51
othaderek <otha.hernandez@gmail.com>
2020-03-13 09:32:06 -0400

    Enqueue and Dequeue methods started.

---
+++ data-structures/queue.js
@@ -0,0 +1,22 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    enqueue(value){
+
+    }
+
+    dequeue(){
+
+    }
+}
