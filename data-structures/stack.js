8880c232a2f3afb8836ce7b295cd17784dfe6d1e
othaderek <otha.hernandez@gmail.com>
2020-03-12 21:33:50 -0400

    Added pop in Stack class.

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -25,7 +25,14 @@ class Stack{
     }
 
     pop(){
-
+        if (!this.first) return null
+        let temp = this.first
+        if (this.first === this.last){
+            this.last = null
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.value
     }
 }
 
