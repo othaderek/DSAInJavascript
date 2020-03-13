eb5db9bfc34bf66005ed62937b9165a4178923ff
othaderek <otha.hernandez@gmail.com>
2020-03-13 10:13:13 -0400

    Completed enqueue method to queue.

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -13,10 +13,27 @@ class Queue{
     }
 
     enqueue(value){
-
+        let newNode = new Node(value)
+        console.log(newNode);
+        
+        if (!this.first){
+            this.first = newNode
+            this.last = newNode
+        } else{
+            this.last.next = newNode
+            this.last = newNode  
+        }
+        this.size++
+        return this
     }
 
     dequeue(){
 
     }
 }
+
+let queue = new Queue()
+
+queue.enqueue(1)
+queue.enqueue(2)
+console.log(queue.enqueue(3));
