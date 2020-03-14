6671b51ea4569b5dab6987ed469787ba288cbfbe
othaderek <otha.hernandez@gmail.com>
2020-03-14 11:26:28 -0400

    BST Insert. Will add notes later.

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -10,4 +10,36 @@ class BinarySearchTree{
     constructor(){
         this.root = null
     }
+
+    insert(value){
+        let newNode = new Node(value)
+        if (!this.root){
+            this.root = newNode
+            return this
+        }
+        let currentNode = this.root
+        while (true){
+            if (value === currentNode.value) return undefined;
+            if (value < currentNode.value){
+                if (currentNode.left === null){
+                    currentNode.left = newNode
+                    return this
+                }
+                currentNode = currentNode.left
+            } else {
+                if (currentNode.right === null){
+                    currentNode.right = newNode
+                    return this
+                }
+                currentNode = currentNode.right
+            }
+        }
+    }
 }
+
+let bst = new BinarySearchTree()
+
+console.log(bst.insert(10));
+console.log(bst.insert(3));
+console.log(bst.insert(39));
+console.log(bst.insert(9));
