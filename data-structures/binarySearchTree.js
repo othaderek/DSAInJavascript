b28fae82f5190d28d1e3f147725236edf4cd9f6a
othaderek <otha.hernandez@gmail.com>
2020-03-13 21:29:00 -0400

    Created BST class.

---
+++ data-structures/binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.right = null
+        this.left = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+}
