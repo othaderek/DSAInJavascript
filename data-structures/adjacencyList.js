c921b41dd42eadef299241f1bcc2e7d988e270c3
othaderek <otha.hernandez@gmail.com>
2020-03-25 22:30:16 -0400

    remove vertex solution, close enough.

--- data-structures/adjacencyList.js
+++ data-structures/adjacencyList.js
@@ -13,6 +13,19 @@ class Graph {
         this.adjacencyList[vertexOne].push(vertexTwo);
         this.adjacencyList[vertexTwo].push(vertexOne);
     }
+
+    removeEdge(vertexOne, vertexTwo){
+        let indexToRemoveOne = this.adjacencyList[vertexOne].indexOf(vertexTwo);
+        this.adjacencyList[vertexOne].splice(indexToRemoveOne);
+        let indexToRemoveTwo = this.adjacencyList[vertexTwo].indexOf(vertexOne);
+        this.adjacencyList[vertexTwo].splice(indexToRemoveTwo);
+        
+    }
+
+    removeVertex(vertex) {
+        for(let edge of this.adjacencyList[vertex]) this.removeEdge(vertex, edge);
+        delete this.adjacencyList[vertex]
+      }
 }
 
 let g = new Graph();
@@ -20,6 +33,14 @@ let g = new Graph();
 g.addVertex('name');
 g.addVertex('age');
 g.addVertex('life');
+g.addVertex('cat');
 g.addEdge('name', 'life');
+g.addEdge('name', 'age');
+g.addEdge('name', 'cat');
 console.log(g);
+console.log(g.removeVertex('name'));
+ 
+console.log(g.adjacencyList);
+
+
 
